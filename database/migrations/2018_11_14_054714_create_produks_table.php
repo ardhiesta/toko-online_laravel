8204136776ad01dbd4508173e7ec4b19a6db19c1
ardhiesta <ardhi.mail@gmail.com>
2018-11-15 10:48:18 +0700

    first commit

---
+++ database/migrations/2018_11_14_054714_create_produks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProduksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id_produk');
+            $table->string('nama_produk');
+            $table->unsignedInteger('id_kategori');
+            $table->foreign('id_kategori')->references('id_kategori')->on('kategori_produk');
+            $table->integer('harga');
+            $table->longText('deskripsi');
+            $table->string('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produk');
+    }
+}
