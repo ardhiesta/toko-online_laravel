47e93243560d57f18aa791e3ea123f6851257ef8
ardhiesta <ardhi.mail@gmail.com>
2019-01-27 11:35:46 +0700

    fix tombol

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -30,7 +30,7 @@
 				</div>
 				<div class="row">
 					<div class="col-md-4"></div>
-					<div class="form-group col-md-2">
+					<div class="form-group col-md-3">
 						<button type="submit" class="btn btn-success">Tambah Kategori Produk</button>
 					</div>
 					<div class="form-group col-md-2">
--- resources/views/kategori/edit.blade.php
+++ resources/views/kategori/edit.blade.php
@@ -31,7 +31,7 @@
 				</div>
 				<div class="row">
 					<div class="col-md-4"></div>
-					<div class="form-group col-md-2">
+					<div class="form-group col-md-3">
 						<button type="submit" class="btn btn-success">Simpan Kategori Produk</button>
 					</div>
 					<div class="form-group col-md-2">
